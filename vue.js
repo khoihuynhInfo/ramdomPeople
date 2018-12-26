ba1ae791467a78738991db1eea967e1e24cf7496
HP\ADMIN <khoi.huynh@infodation.vn>
2018-12-26 23:37:33 +0700

    first commit

---
+++ vue.js
@@ -0,0 +1,35 @@
+var demo = new Vue({
+    el: '#list-people',
+    data: {
+        isLoading: true,
+        peoples: []
+    },
+    created: function () {
+        // after view loaded and readly
+        // call api at here so fire
+        this.fetchPeoples();
+    },
+    methods: {
+        fetchPeoples: async function () {
+            try {
+                const response = await fetch(`
+                    https://randomuser.me/api/?results=15
+                `);
+                const responseJson = await response.json();
+                this.peoples = [...responseJson.results];
+            } catch (error) {
+                throw new Error(error);
+            }
+        }
+    },
+    watch: {
+        peoples: function (newList) {
+            console.log(newList);
+            (typeof newList === 'object' && newList) && (setTimeout(() => {
+                this.isLoading = false;
+            }, 1000));
+        }
+    }
+})
+
+
